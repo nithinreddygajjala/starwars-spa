d7e6c2d8349d1362ebfc22eebd72f713bc67682b
Nithin Gajjala <nithin.gajjala@FIADMIN.COM>
2019-06-12 15:59:12 -0700

    Added Bootstrap Responsive Styles

--- src/app/dashboard/dashboard.component.ts
+++ src/app/dashboard/dashboard.component.ts
@@ -2,47 +2,43 @@ import { Component, OnInit } from '@angular/core';
 import { CharactersService } from '../services/characters.service';
 import { Router, ActivatedRoute, Params } from '@angular/router';
 @Component({
-  selector: 'app-dashboard',
-  templateUrl: './dashboard.component.html',
-  styleUrls: ['./dashboard.component.css']
+	selector: 'app-dashboard',
+	templateUrl: './dashboard.component.html',
+	styleUrls: ['./dashboard.component.css']
 })
 export class DashboardComponent implements OnInit {
 
-  constructor(private characterService: CharactersService, private router: Router, private route: ActivatedRoute ) { }
-  characters: any = [];
-  searchString: any;
-  searchReturnedNoResults: any;
-  filtererdCharacters: any = [];
-  ngOnInit() {
-this.characters = this.characterService.characters;
-this.route.queryParams
+	constructor(private characterService: CharactersService, private router: Router, private route: ActivatedRoute) { }
+	characters: any = [];
+	searchString: any;
+	searchReturnedNoResults: any;
+	filtererdCharacters: any = [];
+	ngOnInit() {
+		this.characters = this.characterService.characters;
+		this.route.queryParams
 			.subscribe(
 				(params: Params) => {
-          console.log(this.route.snapshot.queryParams.hasOwnProperty('q'));
-          if(this.route.snapshot.queryParams.hasOwnProperty('q')) this.searchString = this.route.snapshot.queryParams.q; 
-        console.log('reached');
-        this.filterRoleList()
-        
+					if (this.route.snapshot.queryParams.hasOwnProperty('q')) this.searchString = this.route.snapshot.queryParams.q;
+					this.filterList()
+
 				});
-  }
-  test(event) {
-    this.router.navigate([], {
-      queryParams: {q: event.target.value},
-      relativeTo: this.route,
-      skipLocationChange: false
-      });
-    
-    console.log(event.target.value);
-  }
-
-  navigate(x){
-	this.router.navigate([x.id], {
-		relativeTo: this.route,
-		skipLocationChange: false
+	}
+	search(event) {
+		this.router.navigate([], {
+			queryParams: { q: event.target.value },
+			relativeTo: this.route,
+			skipLocationChange: false
 		});
-  }
+	}
 
-  filterRoleList() {
+	navigate(x) {
+		this.router.navigate([x.id], {
+			relativeTo: this.route,
+			skipLocationChange: false
+		});
+	}
+
+	filterList() {
 		this.searchReturnedNoResults = false;
 		let qText = this.route.snapshot.queryParams['q'];
 
@@ -50,7 +46,6 @@ this.route.queryParams
 			if (this.characters !== undefined) {
 				this.filtererdCharacters = [...this.characters];
 			}
-
 			this.searchReturnedNoResults = false;
 			return;
 		}
@@ -59,25 +54,25 @@ this.route.queryParams
 		let tokens = qText.split(' ').map(tok => tok.toLowerCase());
 		this.filtererdCharacters = [];
 
-		this.characters.forEach((role) => {
+		this.characters.forEach((ele) => {
 			let tokenMatchedCount = 0;
 			tokens.forEach((token: string) => {
-				let isRoleAdded = false;
-				if (role.name !== undefined && role.name.toLowerCase().includes(token)) {
+				let isAdded = false;
+				if (ele.name !== undefined && ele.name.toLowerCase().includes(token)) {
 					tokenMatchedCount++;
-					isRoleAdded = true;
+					isAdded = true;
 				}
-				if (!isRoleAdded && role.company !== undefined && role.company.toLowerCase().includes(token)) {
+				if (!isAdded && ele.company !== undefined && ele.company.toLowerCase().includes(token)) {
 					tokenMatchedCount++;
-					isRoleAdded = true;
+					isAdded = true;
 				}
-				if (!isRoleAdded && role.description !== undefined && role.description.toLowerCase().includes(token)) {
+				if (!isAdded && ele.description !== undefined && ele.description.toLowerCase().includes(token)) {
 					tokenMatchedCount++;
-					isRoleAdded = true;
+					isAdded = true;
 				}
 			});
 			if (tokenMatchedCount === tokens.length) {
-				this.filtererdCharacters.push(role);
+				this.filtererdCharacters.push(ele);
 			}
 		});
 		if (this.filtererdCharacters.length <= 0 && tokens.length > 0 && this.characters.length > 0) {
